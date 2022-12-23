659b79463b45d97e5f09201a296fdbd6de192dd6
Joseph Lee <cgjosephlee@gmail.com>
2022-12-23 10:17:22 +0800

    bump version v0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fasttext-classifier",
-    version="0.0.1",
+    version="0.0.2",
     url="https://github.com/cgjosephlee/FastText-Classifier-Pytorch",
     author="Joseph Lee",
     author_email="cgjosephlee@gmail.com",
